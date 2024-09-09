5f9b5934dd683c89689b3b1d805e21d91e50e20c
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-08 20:46:45 -0600

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { AuthProvider } from '../contexts/AuthContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
