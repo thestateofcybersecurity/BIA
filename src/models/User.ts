55298beb164ba8b16b2a6cafb9a5251b82f4b9a8
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-08 21:24:13 -0600

    Create User.ts

---
+++ src/models/User.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+  },
+}, { timestamps: true })
+
+export const User = mongoose.models.User || mongoose.model('User', UserSchema)
