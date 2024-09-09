7f33d2de14ce9afb1733510f9445c9107ee38a33
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-08 21:50:54 -0600

    Update dashboard.tsx

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useAuth } from '@/contexts/AuthContext'
 import { Box, Button, Heading, VStack, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
