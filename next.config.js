bcbb2b1f859224e4cc12d6c49de4d3f95c1b6ac5
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-08 22:22:21 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  serverRuntimeConfig: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
 }
 
 module.exports = nextConfig
