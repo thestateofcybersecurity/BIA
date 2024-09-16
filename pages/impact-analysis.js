29155797498f892957005de02b8738341253a201
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-16 16:21:12 -0600

    Update impact-analysis.js

--- pages/impact-analysis.js
+++ pages/impact-analysis.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Header from '../components/Header';
 
-const BusinessProcessForm = () => {
+const ImpactAnalysisForm = () => {
   const { user, getAccessTokenSilently } = useAuth0();
   const [formData, setFormData] = useState({
     processName: '',
