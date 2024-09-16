d6b0affe38dc1a27f4b2b05e13e2c456db21af24
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-16 15:58:43 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Header = () => {
@@ -9,16 +9,16 @@ const Header = () => {
     <header>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/business-process">Business Process</Link></li>
-          <li><Link to="/impact-analysis">Impact Analysis</Link></li>
-          <li><Link to="/rto-rpo-analysis">RTO/RPO Analysis</Link></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/business-process">Business Process</Link></li>
+          <li><Link href="/impact-analysis">Impact Analysis</Link></li>
+          <li><Link href="/rto-rpo-analysis">RTO/RPO Analysis</Link></li>
         </ul>
       </nav>
       {isAuthenticated ? (
         <div>
           <span>Welcome, {user.name}!</span>
-          <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
+          <button onClick={() => logout({ returnTo: typeof window !== 'undefined' ? window.location.origin : '' })}>Log Out</button>
         </div>
       ) : (
         <button onClick={() => loginWithRedirect()}>Log In</button>
