2147c447e4ed4d703a8de979df307ae505fe5d9e
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-16 16:31:26 -0600

    Update rto-rpo-analysis.js

--- pages/rto-rpo-analysis.js
+++ pages/rto-rpo-analysis.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Header from '../components/Header';
+import ProtectedRoute from '../components/ProtectedRoute';
 
 const RTORPOAnalysis = () => {
   const { user, getAccessTokenSilently } = useAuth0();
