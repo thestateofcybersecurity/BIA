1f0a0c563b00f918f1e25c3a491c1029eb8895d0
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-16 14:46:40 -0600

    Create Loading.js

---
+++ client/src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loading = () => (
+  <div className="loading">
+    <div className="spinner"></div>
+    <p>Loading...</p>
+  </div>
+);
+
+export default Loading;
