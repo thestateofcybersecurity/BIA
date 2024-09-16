a5a93203e82c38d490ac3b7031ade2ac55cc9d6c
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-16 16:01:41 -0600

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,16 +1,32 @@
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+import { promisify } from 'util';
+import jsonwebtoken from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
 
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
+const client = jwksClient({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
 });
 
-module.exports = { checkJwt };
+const getSigningKey = promisify(client.getSigningKey);
+
+export const checkJwt = async (req, res) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    throw new Error('No token provided');
+  }
+
+  try {
+    const decodedToken = jsonwebtoken.decode(token, { complete: true });
+    const signingKey = await getSigningKey(decodedToken.header.kid);
+    const publicKey = signingKey.getPublicKey();
+
+    const verified = jsonwebtoken.verify(token, publicKey, {
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+      algorithms: ['RS256']
+    });
+
+    req.user = verified;
+  } catch (error) {
+    throw new Error('Invalid token');
+  }
+};
