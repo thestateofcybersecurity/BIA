ade4bacd781ca6501999483ed4ae8a3e94aafafd
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-16 16:26:35 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    NEXT_PUBLIC_AUTH0_DOMAIN: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
+    NEXT_PUBLIC_AUTH0_CLIENT_ID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
+    NEXT_PUBLIC_AUTH0_AUDIENCE: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
   },
 };
