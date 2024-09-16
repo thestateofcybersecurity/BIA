72fdb04f3242bc7e019a22af6aa0138bf2906cc4
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-16 16:26:11 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Auth0Provider } from '@auth0/auth0-react';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ErrorBoundary>
+      <Auth0Provider
+        domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+        clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+        redirectUri={typeof window !== 'undefined' ? window.location.origin : ''}
+        audience={process.env.NEXT_PUBLIC_AUTH0_AUDIENCE}
+      >
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Auth0Provider>
+    </ErrorBoundary>
   );
 }
 
