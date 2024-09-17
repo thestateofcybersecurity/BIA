5991d02bcc381f5a37cce63351426fe3cb17c394
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 14:54:08 -0600

    Update auth0-config.js

--- auth0-config.js
+++ auth0-config.js
@@ -1,9 +1,11 @@
-import { initAuth0 } from '@auth0/nextjs-auth0';
+import { getSession } from '@auth0/nextjs-auth0';
 
-export default initAuth0({
-  secret: process.env.AUTH0_SECRET,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  baseURL: process.env.AUTH0_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-});
+export const getSessionAndUser = async (req, res) => {
+  try {
+    const session = await getSession(req, res);
+    return { session, user: session?.user };
+  } catch (error) {
+    console.error('Error getting session:', error);
+    return { session: null, user: null };
+  }
+};
