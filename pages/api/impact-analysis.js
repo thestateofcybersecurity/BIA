4bb21f1dc255ec9e1fd1c7a64c15ddc87d4c225c
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 12:46:21 -0600

    Create impact-analysis.js

---
+++ pages/api/impact-analysis.js
@@ -0,0 +1,34 @@
+import dbConnect from '../../../lib/dbConnect';
+import ImpactAnalysis from '../../../models/ImpactAnalysis';
+import { getSession } from '@auth0/nextjs-auth0';
+
+export default async function handler(req, res) {
+  const { user } = await getSession(req, res);
+
+  await dbConnect();
+
+  switch (req.method) {
+    case 'POST':
+      try {
+        const impactAnalysis = await ImpactAnalysis.create({
+          userId: user.sub,
+          ...req.body
+        });
+        res.status(201).json({ success: true, data: impactAnalysis });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case 'GET':
+      try {
+        const impactAnalyses = await ImpactAnalysis.find({ userId: user.sub });
+        res.status(200).json({ success: true, data: impactAnalyses });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
