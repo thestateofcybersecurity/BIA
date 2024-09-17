f61469bb23a62ffa1a4b6c71a9f88ff378c8873d
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 14:48:33 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,16 @@
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { useRouter } from 'next/router';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import ErrorBoundary from '../components/ErrorBoundary';
-import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  const onRedirectCallback = (appState) => {
-    router.push(appState?.returnTo || '/');
-  };
-
   return (
     <ErrorBoundary>
-      <Auth0Provider
-        domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
-        clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-        redirectUri={typeof window !== 'undefined' ? window.location.origin : ''}
-        onRedirectCallback={onRedirectCallback}
-        audience={process.env.NEXT_PUBLIC_AUTH0_AUDIENCE}
-      >
+      <UserProvider>
         <ChakraProvider>
-        <UserProvider>
           <Component {...pageProps} />
-        </UserProvider>
         </ChakraProvider>
-      </Auth0Provider>
+      </UserProvider>
     </ErrorBoundary>
   );
 }
