badc378111d13ac1c045c455c422f6f948d6bc9a
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 14:14:35 -0600

    Update [id].js

--- pages/api/business-processes/[id].js
+++ pages/api/business-processes/[id].js
@@ -1,16 +1,13 @@
 // pages/api/business-processes/[id].js
-import { getSession } from '@auth0/nextjs-auth0';
+import { useAuth0 } from '@auth0/auth0-react';
 import connectDB from '../../../config/database';
 import BusinessProcess from '../../../models/BusinessProcess';
 
 export default async function handler(req, res) {
-  const session = await getSession(req, res);
-  if (!session) {
-    return res.status(401).json({ error: 'Not authenticated' });
-  }
-
+const { user, getAccessTokenSilently } = useAuth0();
+  
   const { id } = req.query;
-  await dbConnect();
+  await connectDB();
 
   switch (req.method) {
     case 'PUT':
