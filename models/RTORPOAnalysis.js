33434514665033f408fcd0a79c527d747bfd92f2
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 16:52:27 -0600

    Update RTORPOAnalysis.js

--- models/RTORPOAnalysis.js
+++ models/RTORPOAnalysis.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 const RTORPOAnalysisSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   businessProcessId: { type: mongoose.Schema.Types.ObjectId, ref: 'BusinessProcess', required: true },
+  processName: { type: String, required: true },
   type: { type: String, enum: ['recovery', 'repatriation'], required: true },
   metric: { type: String, enum: ['rto', 'rpo'], required: true },
   acceptableTime: { type: Number, required: true },
