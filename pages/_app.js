6926c7e5ccc3091807088fb69ec7c3b87ad0fbca
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 23:11:22 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import ErrorBoundary from '../components/ErrorBoundary';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <ChakraProvider>
         <ErrorBoundary>
+          <Header />
           <Component {...pageProps} />
         </ErrorBoundary>
       </ChakraProvider>
