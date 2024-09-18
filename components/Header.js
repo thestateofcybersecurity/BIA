c1040c5b199820a2a8fe81e5eeaef9bef0578cb8
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 21:17:19 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Flex, Box, Button, Link as ChakraLink, Text } from '@chakra-ui/react';
+import { Flex, Box, Button, Link as ChakraLink, Text, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 const Header = () => {
@@ -15,10 +16,20 @@ const Header = () => {
     <Flex as="header" bg="blue.500" p={4} justifyContent="space-between" alignItems="center">
       <Box>
         <ChakraLink href="/" color="white" fontSize="xl" fontWeight="bold" mr={6}>Home</ChakraLink>
-        <ChakraLink href="/business-process" color="white" mr={6}>Business Process</ChakraLink>
-        <ChakraLink href="/impact-analysis" color="white" mr={6}>Impact Analysis</ChakraLink>
-        <ChakraLink href="/rto-rpo-analysis" color="white" mr={6}>RTO/RPO Analysis</ChakraLink>
-        <ChakraLink href="/comparative-analysis" color="white">Comparative Analysis</ChakraLink>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} variant="ghost" color="white">
+            Tools
+          </MenuButton>
+          <MenuList>
+            <MenuItem as={Link} href="/business-process">Business Process</MenuItem>
+            <MenuItem as={Link} href="/impact-analysis">Impact Analysis</MenuItem>
+            <MenuItem as={Link} href="/recovery-workflow">Recovery Workflow</MenuItem>
+            <MenuItem as={Link} href="/rto-rpo-analysis">RTO/RPO Analysis</MenuItem>
+            <MenuItem as={Link} href="/maturity-scorecard">Maturity Scorecard</MenuItem>
+            <MenuItem as={Link} href="/tabletop-scenario">Tabletop Scenario</MenuItem>
+            <MenuItem as={Link} href="/comparative-analysis">Comparative Analysis</MenuItem>
+          </MenuList>
+        </Menu>
       </Box>
       <Box>
         {user ? (
