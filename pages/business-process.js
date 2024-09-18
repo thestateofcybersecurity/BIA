f74d686fe4222b3b912c36f2330c6240824502c6
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 21:39:11 -0600

    Update business-process.js

--- pages/business-process.js
+++ pages/business-process.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { Box, Heading, VStack } from '@chakra-ui/react';
+import { Box, Heading, VStack, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import BusinessProcessWorkflow from '../components/BusinessProcessWorkflow';
 import BusinessProcessList from '../components/BusinessProcessList';
 import CSVUpload from '../components/CSVUpload';
 
 const BusinessProcessPage = () => {
   const { user, error, isLoading } = useUser();
+  const [processes, setProcesses] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      fetchProcesses();
+    }
+  }, [user]);
+
+  const fetchProcesses = async () => {
+    // Implement fetchProcesses function
+    // This should fetch processes from your API and update the processes state
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -15,9 +28,24 @@ const BusinessProcessPage = () => {
       <Box className="container" bg="white" p={6} rounded="md" shadow="md">
         <VStack spacing={6} align="stretch">
           <Heading as="h2" size="lg">Business Process Management</Heading>
-          <BusinessProcessList />
-          <Heading as="h3" size="md">Bulk Upload</Heading>
-          <CSVUpload onUploadComplete={() => {}} />
+          <Tabs>
+            <TabList>
+              <Tab>Add New Process</Tab>
+              <Tab>Existing Processes</Tab>
+              <Tab>Bulk Upload</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <BusinessProcessWorkflow onProcessAdded={fetchProcesses} />
+              </TabPanel>
+              <TabPanel>
+                <BusinessProcessList processes={processes} onProcessUpdated={fetchProcesses} />
+              </TabPanel>
+              <TabPanel>
+                <CSVUpload onUploadComplete={fetchProcesses} />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </VStack>
       </Box>
     </Box>
