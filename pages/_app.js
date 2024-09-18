77734ffff184147034ba1e47180585a66feec7bc
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-17 22:57:10 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,30 @@
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import ErrorBoundary from '../components/ErrorBoundary';
 import Header from '../components/Header';
+import { useRouter } from 'next/router';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const { user, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  const isPublicPage = ['/login', '/api/auth/login', '/api/auth/logout', '/api/auth/callback'].includes(router.pathname);
+
+  if (!user && !isPublicPage) {
+    router.push('/login');
+    return null;
+  }
+
   return (
-    <ErrorBoundary>
-      <UserProvider>
-        <ChakraProvider>
-          <Header />
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </UserProvider>
-    </ErrorBoundary>
+    <UserProvider>
+      <ChakraProvider>
+        {user && !isPublicPage && <Header />}
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserProvider>
   );
 }
 
