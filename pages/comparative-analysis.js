887088c5213be6de55f57dd2c7df4217e19ae4c2
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-10-20 16:29:00 -0600

    Update comparative-analysis.js

--- pages/comparative-analysis.js
+++ pages/comparative-analysis.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Box, Heading } from '@chakra-ui/react';
+import { useUser } from '@auth0/nextjs-auth0/client';
 import ComparativeAnalysis from '../components/ComparativeAnalysis';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useRouter } from 'next/router';
 
 const ComparativeAnalysisPage = () => {
+  const { user, error, isLoading } = useUser();
+  const router = useRouter();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (!user) {
+    typeof window !== 'undefined' && router.push('/api/auth/login');
+    return null;
+  }
+
   return (
     <Box p={5}>
       <Heading mb={6}>Comparative Analysis</Heading>
@@ -12,4 +24,4 @@ const ComparativeAnalysisPage = () => {
   );
 };
 
-export default withPageAuthRequired(ComparativeAnalysisPage);
+export default ComparativeAnalysisPage;
